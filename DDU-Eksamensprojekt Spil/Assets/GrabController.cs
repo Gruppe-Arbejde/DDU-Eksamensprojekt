516bd156c6aa630f51f2ec328c90172383d158d8
Maximus Juul <Maximusjjuul@gmail.com>
2022-03-15 14:30:24 +0100

    Object holder

---
+++ DDU-Eksamensprojekt Spil/Assets/GrabController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabController : MonoBehaviour
+{
+    public Transform grabDetect;
+    public Transform boxHolder;
+    public float rayDist;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- DDU-Eksamensprojekt Spil/Assets/Scripts/PlayerMovement.cs
+++ DDU-Eksamensprojekt Spil/Assets/Scripts/PlayerMovement.cs
@@ -5,17 +5,18 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
-
     public Rigidbody2D rb;
+    Vector2 movement;
 
     // Update is called once per frame
     void Update()
     {
-        Input.GetAxisRaw("Horizontal");
+        movement.x =  Input.GetAxisRaw("Horizontal"); //gives value between -1 and 1 depending on what key is pressed
+        movement.y = Input.GetAxisRaw("Vertical"); //gives value between -1 and 1 depending on what key is pressed
     }
 
-    void FixedUpdate()
+    void FixedUpdate() //as update can have fluctuating frame rate fixed update is used instead as it is always called 50 times a second
     {
-        // Movement
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime); //fixedDeltaTime is to assure a constant movement speed
     }
 }
