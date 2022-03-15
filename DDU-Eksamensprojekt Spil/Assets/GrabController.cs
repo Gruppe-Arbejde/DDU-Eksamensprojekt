33680320c0c180c61fc13e6695b30693b371763f
Maximus Juul <Maximusjjuul@gmail.com>
2022-03-15 14:52:56 +0100

    Grabbing is now enabled
    Du kan nu tage objekter, og trække dem med dig

--- DDU-Eksamensprojekt Spil/Assets/GrabController.cs
+++ DDU-Eksamensprojekt Spil/Assets/GrabController.cs
@@ -11,6 +11,21 @@ public class GrabController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        RaycastHit2D grabCheck = Physics2D.Raycast(grabDetect.position, Vector2.right *  transform.localScale, rayDist);
+
+        if(grabCheck.collider != null && grabCheck.collider.tag == "Box")
+        {
+            if (Input.GetKey(KeyCode.G))
+            {
+                grabCheck.collider.gameObject.transform.parent = boxHolder;
+                grabCheck.collider.gameObject.transform.position = boxHolder.position;
+                grabCheck.collider.gameObject.GetComponent<Rigidbody2D>().isKinematic = true;
+            }
+            else
+            {
+                grabCheck.collider.gameObject.transform.parent = null;
+                grabCheck.collider.gameObject.GetComponent<Rigidbody2D>().isKinematic = false;
+            }
+        }
     }
 }
