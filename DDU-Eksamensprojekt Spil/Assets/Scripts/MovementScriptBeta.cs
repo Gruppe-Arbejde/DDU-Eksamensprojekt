0b2f88e22002c2949efffea01432ebf3256185cc
niko391a <69769473+niko391a@users.noreply.github.com>
2022-03-25 14:41:44 +0100

    Insanity implemented
    Changed unity rendering pipeline to URP

---
+++ DDU-Eksamensprojekt Spil/Assets/Scripts/MovementScriptBeta.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FogOfWar : MonoBehaviour
+{
+
+    public float movementSpeed = 8f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f) * movementSpeed * Time.deltaTime;
+        transform.position += movement;
+    }
+}
