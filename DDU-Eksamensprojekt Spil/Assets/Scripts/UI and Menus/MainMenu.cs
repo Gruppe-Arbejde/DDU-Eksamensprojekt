223d7c5a91946560fba7000a478d30f6331a6c67
Maximus Juul <Maximusjjuul@gmail.com>
2022-04-07 13:06:45 +0200

    Resolution works.. kinda
    Co-Authored-By: niko391a <69769473+niko391a@users.noreply.github.com>

---
+++ DDU-Eksamensprojekt Spil/Assets/Scripts/UI and Menus/MainMenu.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+
+    public void SetQuality()
+    {
+        ChangeScreenRes();
+    }
+
+    void ChangeScreenRes()
+    {
+        // Getting the name of what button we've pressed
+        string index = UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name;
+
+        switch (index)
+        {
+            case "0":
+                Screen.SetResolution(1152, 648, true);
+                Debug.Log($"{Screen.currentResolution}");
+                break;
+            case "1":
+                Screen.SetResolution(1280, 768, true);
+                Debug.Log($"{Screen.currentResolution}");
+                break;
+            case "2":
+                Screen.SetResolution(1360, 796, true);
+                Debug.Log($"{Screen.currentResolution}");
+                break;
+            case "3":
+                Screen.SetResolution(1920, 1080, true);
+                Debug.Log($"{Screen.currentResolution}");
+                break;
+
+        }
+
+    }
+
+
+
+}
